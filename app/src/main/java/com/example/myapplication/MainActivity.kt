0c96d56b916c7f02d3de536a90cb1edccfc4410b
G-moog <kingmumuop@gmail.com>
2023-10-11 14:27:38 +0900

    리니어 리사이클러 완성(page 변수화 안되서 10개만 출력됨)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.myapplication
-
+import androidx.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: ActivityMainBinding
 
     private val fooAdapter by lazy {
         FooAdapter {
@@ -18,12 +18,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
 
         // lateinit
         // fooAdapter = FooAdapter {}
         binding.recyclerView.adapter = fooAdapter
         fooAdapter.addItems(Foo.createSamples(0))
+
+
     }
 }
