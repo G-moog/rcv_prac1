cfbe953327abccdf036bc13175c85a52780c8d98
G-moog <kingmumuop@gmail.com>
2023-10-11 14:52:25 +0900

    드롭다운 기능없이 목록만 나오게 함

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 import androidx.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -28,6 +31,22 @@ class MainActivity : AppCompatActivity() {
         // fooAdapter = FooAdapter {}
         binding.recyclerView.adapter = fooAdapter
         fooAdapter.addItems(Foo.createSamples(0))
+
+        val list = listOf("킹냥이","댕댕이")
+
+        binding.spinner.adapter = ArrayAdapter(
+            this, // context
+            android.R.layout.simple_spinner_item, // 기본
+            list // items
+        )
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                // position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
 
 
     }
